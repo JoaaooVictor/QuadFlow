dc24a2bdbae985e26dcfb0005dffb76420570fba
João Victor <joao.v.l.s1998@outlook.com>
2026-07-10 01:01:30 -0300

    tests: escrevendo testes de unidade para criação do usuário.

--- Modules/User/Tests/UnitTests/Application/UserTests.cs
+++ Modules/User/Tests/UnitTests/Application/UserTests.cs
@@ -32,12 +32,10 @@ namespace Tests.UnitTests.Application
 			);
 		}
 
-
 		[Fact]
 		public async Task DeveRegistrarUsuarioQuandoEmailNaoExistir()
 		{
 			// Arrange
-
 			var request = new RegisterUserRequestDto
 			{
 				Name = "João",
@@ -54,17 +52,53 @@ namespace Tests.UnitTests.Application
 				.Returns("senhaHash");
 
 			// Act
-
 			var result = await _useCase.Register(request);
 
 			// Assert
-
 			Assert.True(result.Sucess);
 			Assert.Equal("Usuário registrado com sucesso", result.Message);
 
 			_repository.Verify(
 				x => x.CreateUser(It.IsAny<User>()),
 				Times.Once
+			);
+		}
+
+		[Fact]
+		public async Task NaoDeveRegistrarUsuarioQuandoEmailExistir()
+		{
+			// Arrange
+			var registerUserRequest = new RegisterUserRequestDto
+			{
+				Name = "João",
+				Email = "joao@gmail.com",
+				Password = "Teste123"
+			};
+
+			var existingUser = new User(
+				"João",
+				"joao@gmail.com",
+				"hash"
+			);
+
+			_repository
+				.Setup(r => r.GetUserByEmail(registerUserRequest.Email))
+				.ReturnsAsync(existingUser);
+
+			// Act
+			var result = await _useCase.Register(registerUserRequest);
+
+			// Assert
+			Assert.False(result.Sucess);
+
+			Assert.Equal(
+				"Usuário com email informado existente",
+				result.Message
+			);
+
+			_repository.Verify(
+				r => r.CreateUser(It.IsAny<User>()),
+				Times.Never
 			);
 		}
 	}
