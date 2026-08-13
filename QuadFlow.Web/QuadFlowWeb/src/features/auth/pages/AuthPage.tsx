a006c6855ec6d875bdcac5e6d6bd920fe584c2e6
João Victor <joao.v.l.s1998@outlook.com>
2026-08-13 09:02:28 -0300

    feat: inclusão do css da página de auth

--- QuadFlow.Web/QuadFlowWeb/src/features/auth/pages/AuthPage.tsx
+++ QuadFlow.Web/QuadFlowWeb/src/features/auth/pages/AuthPage.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import type { LoginRequestDto } from "../types/auth.types";
 import { Login } from '../services/auth.services'
+import '../styles/AuthPage.css';
+import { useNavigate } from "react-router-dom";
 
 export default function AuthPage() {
-    async function handleLogin(event: React.SubmitEvent<HTMLFormElement>){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    async function handleLogin(event: React.SubmitEvent<HTMLFormElement>) {
         event.preventDefault();
 
         const request: LoginRequestDto = {
@@ -12,42 +19,44 @@ export default function AuthPage() {
         };
 
         const response = await Login(request);
-        
-        if(response.status === 400){
+
+        if (response.status === 400) {
             console.log(response.data.message)
         }
 
-        if(response.status === 200){
+        if (response.status === 200) {
             console.log(response.data.message)
             localStorage.setItem('token', response.data.value.token);
+            navigate('/dashboard');
         }
     };
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
     return (
-        <main>
-            <form onSubmit={handleLogin}>
-                <h1>Bem vindo ao QuadFlow</h1>
-                <div>
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Senha</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Entrar</button>
-            </form>
+        <main className="flex min-h-screen"> 
+            <section className="bg-auth-left">
+            </section>
+            <section className="bg-auth-right">
+                <form onSubmit={handleLogin} className="form-auth p-8">
+                    <h1>Bem vindo ao QuadFlow</h1>
+                    <div>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Senha</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Entrar</button>
+                </form>
+            </section>
         </main>
     );
 };
