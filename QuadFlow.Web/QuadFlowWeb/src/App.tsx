8a42d0ab1dfe685407f81299846746b8076e554e
João Silveira <joao.adm.etec@outlook.com>
2026-09-23 22:29:57 -0300

    feat: add toast no projeto Web e ajuste no status code da controller de usuário

--- QuadFlow.Web/QuadFlowWeb/src/App.tsx
+++ QuadFlow.Web/QuadFlowWeb/src/App.tsx
@@ -1,7 +1,12 @@
 import AppRoutes from "./routes/AppRoutes";
-
+import { ToastContainer } from "react-toastify";
 function App() {
-    return <AppRoutes />;
+    return (
+        <>
+            <AppRoutes />
+            <ToastContainer />
+        </>
+    )
 }
 
 export default App;
--- QuadFlow.Web/QuadFlowWeb/src/features/auth/pages/AuthPage.tsx
+++ QuadFlow.Web/QuadFlowWeb/src/features/auth/pages/AuthPage.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import type { LoginRequestDto } from "../types/auth.types";
 import { Login } from "../services/auth.services";
 import { useNavigate } from "react-router-dom";
-import loginImage  from "../../../assets/login-image.png";
+import loginImage from "../../../assets/login-image.png";
+import { toast } from "react-toastify";
 
 export default function AuthPage() {
     const [email, setEmail] = useState("");
@@ -17,34 +18,37 @@ export default function AuthPage() {
             password,
         };
 
-        const response = await Login(request);
+        try {
+            const response = await Login(request);
 
-        if (response.status === 400) {
-            console.log(response.data.message);
-        }
+            if (!response.data.sucess) {
+                toast.error(response.data.message);
+                return;
+            }
 
-        if (response.status === 200) {
-            console.log(response.data.message);
-            localStorage.setItem("token", response.data.value.token);
-            navigate("/dashboard");
+            if (response.status === 200) {
+                localStorage.setItem("token", response.data.value.token);
+                toast.success(response.data.message);
+                navigate("/dashboard");
+            }
+
+        } catch (error) {
+            toast.error("E-mail ou senha inválidos.");
         }
     }
 
     return (
-        <main className="relative flex min-h-screen overflow-hidden bg-[#3E5C76]">
-            <section className="hidden w-[40%] items-center justify-center bg-[#F0EBD8] p-10 md:flex">
-                <div className="flex h-full w-full max-w-md items-center justify-center rounded-2x1">
-                    <span className="text-center text-sm font-medium text-[#3E5C76]/60">
-                        <img src={loginImage} alt="Ilustração de login"/>
-                    </span>
-                </div>
-            </section>
 
-            <section className="flex w-full items-center justify-center px-6 py-10 md:w-[60%]">
-                <form
-                    onSubmit={handleLogin}
-                    className="w-full max-w-md rounded-2xl bg-[#E0E0E0] p-8 shadow-2xl"
-                >
+        <main className="relative flex min-h-screen overflow-hidden bg-[#3E5C76]">
+            <section className="relative hidden w-[40%] items-center justify-center bg-[#F0EBD8] lg:flex">
+                <img
+                    src={loginImage}
+                    alt="Ilustração de login"
+                    className="absolute right-[-50px] z-10 w-[400px] xl:right-[-80px] xl:w-[700px]"
+                />
+            </section>
+            <section className="flex w-full items-center justify-center px-6 py-10 lg:w-[60%]">
+                <form onSubmit={handleLogin} className="w-full max-w-md rounded-2xl bg-[#E0E0E0] p-8 shadow-2xl">
                     <div className="mb-8">
                         <h1 className="text-3xl font-bold text-[#26384A]">
                             Bem-vindo ao QuadFlow
@@ -100,6 +104,7 @@ export default function AuthPage() {
                         Entrar
                     </button>
                 </form>
+
             </section>
         </main>
     );
